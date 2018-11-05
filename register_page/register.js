ff6920fa9008b53b827c984b60a98878b8b651cf
eddieyyy <35881133+eddieyyy@users.noreply.github.com>
2018-11-05 15:36:15 -0500

    Update register.js

--- register_page/register.js
+++ register_page/register.js
@@ -84,7 +84,7 @@ function all_fields_filled(username, password, confirmPassword, email, checkbox)
 }
 
 function toMainPage(e){
-	window.location.href = "../main_page/.html"
+	window.location.href = "../main_page/index.html"
 }
 
 function clear_error_message(){
